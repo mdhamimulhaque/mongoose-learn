d964a764a26e07b00e2262437a46cd821e94bc50
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-06-10 18:36:27 +0600

    create interface & schrema & interface connect with schrema

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,41 @@ const app:Application = express()
 // ===> using cors
 app.use(cors())
 
+// ===> parse data
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 
 app.get('/', (req:Request, res:Response,next:NextFunction) => {
-    res.send('Hello World!');
-    next()
+    // res.send('Hello World!');
+    // next()
+
+    // ---> inserting a test data into mongodb
+    /*
+    step-1: interface
+    step-2: schema
+    step-3: Model
+    step-4: Database Query
+    */ 
+
+    // ---> step-1:
+    interface Iuser{
+        id:string;
+        role:"student";
+        password:string;
+        name:{
+            firstName:string;
+            middleName:string;
+            lastName:string
+        };
+       dateOfBirth?:string;
+       gender:"male" | "female"; 
+       email?:string;
+       contactNo:string;
+       emergencyContactNo:string;
+       presentAddress:string;
+       permanentAddress:string;
+    }
   })
 
 
