5d3ea4c3b26f3645c1c4441bb67701d1861f59c9
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-06-10 16:57:41 +0600

    more organize structure

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
-const port = 5000
+import mongoose from "mongoose";
+import app from "./app";
+
+const port:number = 5000 
+
 
 // =====> database connection
 async function main() {
     try{
         await mongoose.connect('mongodb://127.0.0.1:27017/learn-mongoose');
+        
+    app.listen(port, () => {
+     console.log(`server listening on port ${port}`)
+  })
     }catch(err){
-        console.log(`database is not connected`)
+        console.log(`database is not connected`,err)
     }
   
   }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  main()
 
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`)
-})
