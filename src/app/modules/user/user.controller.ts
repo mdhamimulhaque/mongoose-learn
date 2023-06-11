75bf7bdb9152057e82fc8e97ceb80d8400c2b3ac
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-06-11 18:21:42 +0600

    create user & post route

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from "express";
-import { createUserToDB } from "./user.service";
+import { createUserToDB, getUsersFromDB } from "./user.service";
 
+//===> pattern::route -> controller -> service
 export const createUser=async(req:Request, res:Response) => {
-    const user = await createUserToDB()
+    const data = req.body;
+    const user = await createUserToDB(data)
     res.status(200).json({
         status:'success',
         data:user
     })
 };
 
+export const getUsers=async(req:Request,res:Response)=>{
+const user = await getUsersFromDB()
+res.status(200).json({
+    status:"success",
+    data:user
+})
+}
 
-//===> pattern::route -> controller -> service
+
+
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,29 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
-export const createUserToDB=async()=>{
-    const user = await new User({
-        id:'13276',
-        role:"student",
-        password:"123456",
-        name:{
-            firstName:"Md",
-            middleName:"Rownok",
-            lastName:"Emon",
-        },
-       dateOfBirth:"5th march",
-       gender:"male",
-       email:"rrr@gmail.com",
-       contactNo:"+023308223",
-       emergencyContactNo:"+023308234",
-       presentAddress:"Jashore",
-       permanentAddress:"Narail"
-    });
+export const createUserToDB=async(payload:IUser):Promise<IUser>=>{
+    const user = new User(payload)
       await user.save();
       return user;
 }
+
+export const getUsersFromDB=async():Promise<IUser[]>=>{
+   const users = await User.find()
+   return users;
+}
+
+// id:'13276',
+// role:"student",
+// password:"123456",
+// name:{
+//     firstName:"Md",
+//     middleName:"Rownok",
+//     lastName:"Emon",
+// },
+// dateOfBirth:"5th march",
+// gender:"male",
+// email:"rrr@gmail.com",
+// contactNo:"+023308223",
+// emergencyContactNo:"+023308234",
+// presentAddress:"Jashore",
+// permanentAddress:"Narail"
